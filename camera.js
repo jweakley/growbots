0e554f12eb20404ce6e52d795674a3b942e84231
Joe Weakley <joe.weakley@digitalaugment.com>
2013-10-03 23:48:22 -0700

    Added camera logging.

--- camera.js
+++ camera.js
@@ -1,24 +1,16 @@
 var RaspiCam = require("raspicam");
 
-var camera = new RaspiCam({ opts });
+var camera = new RaspiCam({
+  mode: 'photo',
+  output: './pictures/test_pic.jpg'
+});
+
 
 //to take a snapshot, start a timelapse or video recording
-camera.start( );
-
-//to stop a timelapse or video recording
-camera.stop( );
-
-//listen for the "started" event triggered when the start method has been successfully initiated
-camera.on("started", function(){
-    //do stuff
-});
+camera.start();
 
 //listen for the "read" event triggered when each new photo/video is saved
-camera.on("read", function(err, filename){
-    //do stuff
-});
-
-//listen for the process to exit when the timeout has been reached
-camera.on("exited", function(){
-    //do stuff
+camera.on("read", function(err, timestamp, filename){
+  if (filename.slice(-1)[0] === '~') return;
+  camera.stop();
 });
--- sample.js
+++ sample.js
@@ -1,10 +1,11 @@
 var five = require('johnny-five'),
-    demeter = require('./lib/demeter.js');
-
+    demeter = require('./lib/demeter.js'),
+    loggingFrequency = 5000,
+    RaspiCam = require("raspicam");
 
 five.Board().on("ready", function() {
-  var loggingFrequency = 1000,
-      growbed = new demeter.Environment(
+
+  var growbed = new demeter.Environment(
         'Main Growbed',
         {
           lightLevel: new five.Sensor({ pin: "A2", freq: loggingFrequency }),
@@ -14,7 +15,12 @@ five.Board().on("ready", function() {
       fishTank = new demeter.Environment(
         'Fish Tank',
         {
-          waterDepth: new five.Sensor({ pin: "A1", freq: loggingFrequency })
+          waterDepth: new five.Sensor({ pin: "A1", freq: loggingFrequency }),
+          camera: new RaspiCam({
+            mode: 'photo',
+            output: './pictures/fish_tank.jpg',
+            timelapse: loggingFrequency
+          })
         }
       );
 });
